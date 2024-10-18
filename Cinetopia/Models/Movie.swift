a68ee28413b9e274c4f98929a64cb9205b979d28
robson.arcoleze <robson.arcoleze@mportal.com.br>
2024-10-18 10:34:33 -0300

    feat: Implementado Struct para Movies

---
+++ Cinetopia/Models/Movie.swift
@@ -0,0 +1,17 @@
+//
+//  Movie.swift
+//  Cinetopia
+//
+//  Created by MAC-0350 Multiportal on 18/10/24.
+//
+
+import Foundation
+
+struct Movie {
+    let id: Int
+    let title: String
+    let image: String
+    let synopsis: String
+    let rate: String
+    let releaseDate: String
+}
